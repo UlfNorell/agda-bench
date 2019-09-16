ed9a4a99c128027bc58032cf1f9ad0b860e65a43
Ulf Norell <ulf.norell@gmail.com>
2019-09-16 09:58:20 +0200

    Add benchmarking of type checking performance

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.Fail (MonadFail)
 import Control.Monad.IO.Class
 import Criterion.Main
 import Data.List
@@ -14,22 +15,26 @@ import Agda.Compiler.Common
 import Agda.Interaction.BasicOps
 import Agda.TypeChecking.Monad
 import Agda.TypeChecking.Pretty
-import Agda.Syntax.Internal hiding (sort)
+import Agda.Syntax.Internal hiding (sort, set)
 import Agda.Syntax.Position (noRange)
 import Agda.Syntax.Translation.ConcreteToAbstract
 import Agda.TheTypeChecker
+import Agda.TypeChecking.Rules.Term (isType_)
 import Agda.TypeChecking.Reduce
+import Agda.Interaction.CommandLine (withCurrentFile)
 import Agda.Utils.Lens
 import Agda.Utils.Pretty (prettyShow)
+import Agda.Utils.FileName
 
 data Options = Options { criterionOptions :: [String]
                        , singleRun        :: Maybe String
                        , customBench      :: [String]
+                       , typeCheckBench   :: [(String, String)]
                        , useCallByName    :: Bool
                        , fullNormalForm   :: Bool }
 
 defaultOptions :: Options
-defaultOptions = Options [] Nothing [] False False
+defaultOptions = Options [] Nothing [] [] False False
 
 moreOpts :: Monad m => String -> Options -> m Options
 moreOpts s opts = return opts{ criterionOptions = criterionOptions opts ++ words s }
@@ -46,6 +51,12 @@ normaliseFlag opts = return opts{ fullNormalForm = True }
 callByNameFlag :: Monad m => Options -> m Options
 callByNameFlag opts = return opts{ useCallByName = True }
 
+typeCheckFlag :: MonadFail m => String -> Options -> m Options
+typeCheckFlag s opts =
+  case break (== ":") (words s) of
+    (e, ":" : t) -> return opts{ typeCheckBench = (unwords e, unwords t) : typeCheckBench opts }
+    _ -> fail "Usage: --type-check \"EXPR : TYPE\""
+
 benchBackend :: Backend' Options () () () ()
 benchBackend = Backend'
   { backendName           = "benchmark"
@@ -57,6 +68,8 @@ benchBackend = Backend'
                                 "Evaluate a single expression"
                             , Option ['C'] ["custom"] (ReqArg addCustomBench "EXPR")
                                 "Add a custom benchmark for EXPR"
+                            , Option ['T'] ["type-check"] (ReqArg typeCheckFlag "EXPR : TYPE")
+                                "Add a custom benchmark to type check EXPR against TYPE"
                             , Option ['n'] ["nf"] (NoArg normaliseFlag)
                                 "Full normalisation instead of weak-head reduction"
                             , Option [] ["call-by-name"] (NoArg callByNameFlag)
@@ -88,6 +101,8 @@ normaliser opts | fullNormalForm opts = strat . normalise
 
 runBenchmarks :: Options -> TCM ()
 runBenchmarks opts@Options{ singleRun = Just s } = printExpr opts 0 s
+runBenchmarks opts@Options{ typeCheckBench = bs @ (_ : _) } =
+  runTypeCheckBenchmarks opts bs
 runBenchmarks opts = do
   benchmarks <- (++ customBench opts) <$> findBenchmarks
   actions    <- mapM (normaliseExpr $ normaliser opts) benchmarks
@@ -99,6 +114,21 @@ runBenchmarks opts = do
       mapM_ (printExpr opts 2) benchmarks
       reportSLn "" 1 ""
   liftIO $ withArgs args $ defaultMainWith defaultConfig $ zipWith mkBench benchmarks actions
+
+runTypeCheckBenchmarks :: Options -> [(String, String)] -> TCM ()
+runTypeCheckBenchmarks opts benchmarks = do
+  let mkBench (e, t) action = bench (e ++ " : " ++ t) (whnfIO action)
+      args = criterionOptions opts
+  actions <- mapM typeCheckExpr benchmarks
+  liftIO $ withArgs args $ defaultMainWith defaultConfig $ zipWith mkBench benchmarks actions
+
+typeCheckExpr :: (String, String) -> TCM (IO ())
+typeCheckExpr (e, t) = do
+  e <- concreteToAbstract_ =<< parseExpr noRange e
+  t <- isType_ =<< concreteToAbstract_ =<< parseExpr noRange t
+  withCurrentFile $ tcmToIO $ do
+    v <- checkExpr e t
+    seq v $ return ()
 
 tcmToIO :: TCM a -> TCM (IO a)
 tcmToIO m = TCM $ \ s e -> return (unTCM m s e)
